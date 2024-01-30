c37f873eb3d09afd099e707970c94801d89464b9
Minji Lee <minji0479@gmail.com>
2024-01-30 17:58:45 +0900

    setting java file

---
+++ BJ/src/SwExpertAcademy/D4_Laffer1.java
@@ -0,0 +1,17 @@
+package src.SwExpertAcademy;
+/**
+ * @author 
+ * @date 
+ * @link
+ * @keyword_solution  
+ * @input 
+ * @output   
+ * @time_complex  
+ * @perf 
+ */
+public class D4_Laffer1 {
+
+	public static void main(String args[]) {
+		
+	}
+}
