f6bca075ae2df0a087bdc2f43098c0a2fcad6700
Minji Lee <minji0479@gmail.com>
2024-02-07 14:58:25 +0900

    20240207 S2 18352

--- BJ/src/Silver/S2/S2_18352.java
+++ BJ/src/Silver/S2/S2_18352.java
@@ -1,9 +1,9 @@
 package src.Silver.S2;
 /**
- * @author Miji Lee
+ * @author Minji Lee
  * @date 20230207
  * @link
- * @keyword_solution 인접리스트 활용?
+ * @keyword_solution bfs 레벨 이용해서 풀이
  * @input  
  * 도시의 개수 N, 도로의 개수 M, 거리 정보 K, 출발 도시의 번호 X
  * (2 ≤ N ≤ 300,000, 1 ≤ M ≤ 1,000,000, 1 ≤ K ≤ 300,000, 1 ≤ X ≤ N)
@@ -16,52 +16,79 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class S2_18352 {
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens;
-	static int cityNum,loadNum,K,startCity;
-	static ArrayList<Integer> result = new ArrayList<Integer>();
+	static int cityNum, loadNum, K, startCity;
+	static StringBuilder sb = new StringBuilder();
+	
+	static int[] distance;
 	static ArrayList<Integer>[] list;
+	static Queue<Integer> q = new LinkedList<>();
+	static ArrayList<Integer> result = new ArrayList<>();
+	
 	public static void main(String[] args) throws IOException {
 		tokens = new StringTokenizer(br.readLine());
 		cityNum = Integer.parseInt(tokens.nextToken());
 		loadNum = Integer.parseInt(tokens.nextToken());
 		K = Integer.parseInt(tokens.nextToken());
 		startCity = Integer.parseInt(tokens.nextToken());
+		
+		distance = new int[cityNum+1];
+		Arrays.fill(distance, -1);
+		
 		list = new ArrayList[cityNum+1];
 		
-		// 인접리스트 만들
-		for(int i =1 ; i <= loadNum; i++) {
+		
+		for(int i = 0; i < loadNum; i++) {
 			tokens = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(tokens.nextToken());
 			int b = Integer.parseInt(tokens.nextToken());
 			if(list[a] == null) {
-				list[a] = new ArrayList<Integer>();
+				list[a] = new ArrayList<>();
 			}
 			list[a].add(b);
 		}
 		
-		checkingCity(startCity, 0);
+		bfs(startCity);
+		
+		for(int i =1; i < distance.length; i++) {
+			if(distance[i] == K) {
+				sb.append(i).append("\n");
+			}
+		}
+		if(sb.length() == 0) {
+			sb.append(-1);
+		}
+		System.out.println(sb);
+	}
+	
+	public static void bfs(int city) {
+		
+		q.offer(city);
+		distance[city] = 0;
+		
+		while(!q.isEmpty()) {
+			int tmp = q.poll();
+			if(list[tmp] != null) {
+				for(int m : list[tmp]) {
+					if(distance[m] == -1) {
+						q.offer(m);
+						distance[m] = distance[tmp] + 1;
+					}
+					
+				}
+			}
+		}
+		
 		
 	}
 	
-	public static void checkingCity(int startCity, int cnt) {
-		if(cnt == K) {
-			result.add(startCity);
-			return;
-		}
-		for(int i = 0; i < list[startCity].size(); i++) {
-			int nextCity = list[startCity].get(i);
-			if(cnt == K) {
-				result.add(nextCity);
-			}
-			if(!list[nextCity].isEmpty()) {
-				checkingCity(nextCity, cnt+1); 
-			}
-			cnt++;
-		}
-	}
-
+	
 }
+	
