251e163363e172610891518cc489c153c937a48f
Minji Lee <minji0479@gmail.com>
2024-02-08 22:23:31 +0900

    20240208 SW academy D3

--- BJ/src/SwExpertAcademy/d3/D3_햄버거다이어트.java
+++ BJ/src/SwExpertAcademy/d3/D3_햄버거다이어트.java
@@ -3,45 +3,36 @@ package src.SwExpertAcademy.d3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 
 public class D3_햄버거다이어트 {
 	static int T, N, L;
-	static Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	static int[] r_score, r_kal;
 	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	static StringTokenizer tokens; 
 	static StringBuilder sb = new StringBuilder();
-	static int resultKal = 0;
-	static int resultScore = 0;
-	static Integer[] keys;
+	static int resultScore;
+
 	
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		T = Integer.parseInt(br.readLine());
 		for(int t = 1; t <=T ; t++) {
-			resultKal = 0;
+			resultScore = 0;
 			tokens = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(tokens.nextToken());
 			L = Integer.parseInt(tokens.nextToken());
 			
-			keys = new Integer[N];
-			
-			for(int i = 1; i <=N; i++) {
+			r_score = new int[N];
+			r_kal = new int[N];
+			for(int i = 0; i < N; i++) {
 				tokens = new StringTokenizer(br.readLine());
-				int key = Integer.parseInt(tokens.nextToken());
-				int value = Integer.parseInt(tokens.nextToken());
-				map.put(key, value);
-				keys[i-1] = key;
+				r_score[i] = Integer.parseInt(tokens.nextToken());
+				r_kal[i] = Integer.parseInt(tokens.nextToken());
+		
 			}
 			
 			boolean[] choose = new boolean[N];
-			
-			Arrays.sort(keys, Collections.reverseOrder());
 			
 			powerSet(0, choose);
 			
@@ -66,25 +57,18 @@ public class D3_햄버거다이어트 {
 	private static void findingMax(boolean[] choose) {
 		int sumKal = 0;
 		int score = 0;
-		int key; 
 		for(int i = 0; i < N ; i++) {
 			if(choose[i] == true) {
-				key = keys[i];
-				sumKal += map.get(key);
-				score += key;
-				if(sumKal > L) {
-					sumKal = -1;
-					break;
-				}
+				score += r_score[i];
+				sumKal += r_kal[i];
 			}
-			
+			if(sumKal > L) {
+				return;
+			}
 		}
-		if(score > resultScore && sumKal != -1) {
-			resultScore = Math.max(resultScore, score);
-		}
+		
+		resultScore = Math.max(resultScore, score);
+		
 		
 	}
-	
-		
-
 }
