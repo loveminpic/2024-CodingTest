3afc36d97ddd83d4d20d3681a8625ca5935f6163
Minji Lee <minji0479@gmail.com>
2024-02-13 00:42:16 +0900

    working

---
+++ BJ/src/Gold/G4/G4_14938.java
@@ -0,0 +1,13 @@
+package src.Gold.G4;
+
+public class G4_14938 {
+	
+	public static void main(String[] args) {
+		
+
+	}
+	
+	public void dijkstra() {
+		
+	}
+}
---
+++ BJ/src/Gold/G4/G4_1753.java
@@ -0,0 +1,21 @@
+package src.Gold.G4;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class G4_1753 {
+	
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer tokens ;
+	static int V, E ;
+	static PriorityQueue<Integer> pq = new PriorityQueue<>();
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
