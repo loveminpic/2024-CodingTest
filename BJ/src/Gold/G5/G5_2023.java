f8d763189b9211edb9ce9707f6a861bdeb6c5731
Minji Lee <minji0479@gmail.com>
2024-02-12 23:01:13 +0900

    coding test

--- BJ/src/Gold/G5/G5_2023.java
+++ BJ/src/Gold/G5/G5_2023.java
@@ -1,4 +1,4 @@
-package src.Gold;
+package src.Gold.G5;
 
 import java.util.Scanner;
 
---
+++ BJ/src/Silver/S1/S1_11286.java
@@ -0,0 +1,10 @@
+package src.Silver.S1;
+
+public class S1_11286 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
