c979a8fca9338570e5f637dd4a3d475f630dbfea
SSAFY <SSAFY@70.12.103.44>
2024-02-16 14:24:08 +0900

    class workshop bfs with adjlist and adjmatrix

--- BJ/src/Class/AdjListTest.java
+++ BJ/src/Class/AdjListTest.java
@@ -1,6 +1,8 @@
 package src.Class;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
 
 // 무향 리스트 연결리스트 버전 
@@ -40,17 +42,40 @@ public class AdjListTest {
 			
 		}
 		
-		for(Node node : adjList) {
-			System.out.println(node);
-		}
+		bfs(adjList,0);
 
 	}
 
+	static void bfs(Node[] adjList, int start) {
+		
+		// 1. 큐와 방문관리 배열 준비 
+		int V = adjList.length;
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean[] visited = new boolean[V];
+		
+		//2. 시작 정점를 큐에 넣고 방문 체크
+		queue.offer(start);
+		visited[start] = true;
+		
+		//3. 큐로 방문체크
+		while(!queue.isEmpty()) {
+			int current = queue.poll(); // 4. 탐색해야하는 정점 꺼내기
+			System.out.println((char) (current+65));
+			
+			// 5. 탐색정점 주변의 인접 정점들이 탐색될 수 있도록 처리하기
+			for(Node tmp = adjList[current]; tmp != null ; tmp = tmp.next) {
+				if(!visited[tmp.to]) {
+					queue.offer(tmp.to);
+					visited[tmp.to] = true;
+				}
+			}
+		}
+	}
 }
 /*
 7
 8
-0 10
+0 1
 0 2
 0 5
 0 6
@@ -58,4 +83,16 @@ public class AdjListTest {
 5 3
 5 4
 6 4
+*/
+/*
+7
+8
+0 1
+0 2
+1 3
+1 4
+2 4
+3 5
+4 5
+5 6
 */
--- BJ/src/Class/AdjMatrixTest.java
+++ BJ/src/Class/AdjMatrixTest.java
@@ -1,6 +1,8 @@
 package src.Class;
 
+import java.util.ArrayDeque;
 import java.util.Arrays;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class AdjMatrixTest {
@@ -18,12 +20,64 @@ public class AdjMatrixTest {
 			
 		}
 		
-		for(int[] row : adjMatrix) {
-			System.out.println(Arrays.toString(row));
-		}
-
+		bfs(adjMatrix,0);
+//		bfs2(adjMatrix,0);
 	}
+	
+	static void bfs(int[][] adjMatrix, int start) {
+		// 1. 큐와 방문관리 배열 준비 
+		int V = adjMatrix.length;
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean[] visited = new boolean[V];
+		
+		//2. 시작 정점를 큐에 넣고 방문 체크
+		queue.offer(start);
+		visited[start] = true;
+		
+		//3. 큐로 방문체크
+		while(!queue.isEmpty()) {
+			int current = queue.poll(); // 4. 탐색해야하는 정점 꺼내기
+			System.out.println((char) (current+65));
+			
+			// 5. 탐색정점 주변의 인접 정점들이 탐색될 수 있도록 처리하기
+			for(int i = 0; i < V; ++i) {
+				if(adjMatrix[current][i] != 0 && !visited[i]) {
+					queue.offer(i);
+					visited[i] = true;
+					
+				}
+			}
+		}
+	}
+	
+	// 큐를 뺄때 방문처리하면 나오는 일. 
 
+	static void bfs2(int[][] adjMatrix, int start) {
+		// 1. 큐와 방문관리 배열 준비 
+		int V = adjMatrix.length;
+		Queue<Integer> queue = new ArrayDeque<>();
+		boolean[] visited = new boolean[V];
+		
+		//2. 시작 정점를 큐에 넣고 방문 체크
+		queue.offer(start);
+//		visited[start] = true;
+		
+		//3. 큐로 방문체크
+		while(!queue.isEmpty()) {
+			int current = queue.poll(); // 4. 탐색해야하는 정점 꺼내기
+			visited[current] = true;
+			System.out.println((char) (current+65));
+			
+			// 5. 탐색정점 주변의 인접 정점들이 탐색될 수 있도록 처리하기
+			for(int i = 0; i < V; ++i) {
+				if(adjMatrix[current][i] != 0 && !visited[i]) {
+					queue.offer(i);
+					
+				}
+
+			}
+		}
+	}
 }
 /*
 7
@@ -36,4 +90,16 @@ public class AdjMatrixTest {
 5 3
 5 4
 6 4
+*/
+/*
+7
+8
+0 1
+0 2
+1 3
+1 4
+2 4
+3 5
+4 5
+5 6
 */
